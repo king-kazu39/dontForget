22a41bcc818511cf7ec3fa525842ec7d77bf59ac
カーズ <magmanakazato@gmail.com>
2018-12-30 22:53:59 +0800

    言語設定画面のViewControllerとswiftファイルを追加

---
+++ myPrApp/LangViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LangViewController.swift
+//  myPrApp
+//
+//  Created by 仲里和也 on 2018/12/30.
+//  Copyright © 2018 Kazuya Nakazato. All rights reserved.
+//
+
+import UIKit
+
+class LangViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
