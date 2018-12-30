1cf8140e3602db3da7c3631a48ead8054b403c27
カーズ <magmanakazato@gmail.com>
2018-12-30 18:09:39 +0800

    Initial Commit

---
+++ myPrApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myPrApp
+//
+//  Created by 仲里和也 on 2018/12/30.
+//  Copyright © 2018 Kazuya Nakazato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
