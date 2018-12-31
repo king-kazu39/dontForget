6b0cd5027523b72e08d37ecce93a0531bd8a246a
カーズ <magmanakazato@gmail.com>
2018-12-31 12:11:11 +0800

    言語設定画面の画像を生成できるようにした

--- myPrApp/LangViewController.swift
+++ myPrApp/LangViewController.swift
@@ -9,14 +9,33 @@
 import UIKit
 
 class LangViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //UIImageViewのインスタンス作成
+        let imageView = UIImageView()
+        
+        //TODO:後で配置を修正する
+        //imageViewの位置とサイズを設定
+        imageView.frame = CGRect(x: self.view.frame.width / 2 - 150, y: self.view.frame.height / 2 - 150, width: 200, height: 200)
+        
+        //画像を生成。
+        let image = UIImage(named:"Langage.png")!
+        
+        //生成した画像をimageViewに設定
+        imageView.image = image
+        
+        //viewにimageViewをsubViewとして追加
+        self.view.addSubview(imageView)
+        
     }
     
-
+    
+    
+    
     /*
     // MARK: - Navigation
 
