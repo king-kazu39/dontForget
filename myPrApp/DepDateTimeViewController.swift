7f5348194322a7d41c2b89f08e7b6ef1d398242d
カーズ <magmanakazato@gmail.com>
2019-01-09 15:58:31 +0800

    時間設定画面（UIViewController）をStoryBoardに用意。画面遷移追加。

---
+++ myPrApp/DepDateTimeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DepDateTimeViewController.swift
+//  myPrApp
+//
+//  Created by 仲里和也 on 2019/01/09.
+//  Copyright © 2019 Kazuya Nakazato. All rights reserved.
+//
+
+import UIKit
+
+class DepDateTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
