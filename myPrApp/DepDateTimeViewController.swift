5636baaaf13ade5c6f62bce5f1b7677d9599d769
カーズ <magmanakazato@gmail.com>
2019-01-13 17:07:02 +0800

    TOP画面に戻るためのアラート画面追加。コメント修正。

--- myPrApp/DepDateTimeViewController.swift
+++ myPrApp/DepDateTimeViewController.swift
@@ -19,7 +19,6 @@ class DepDateTimeViewController: UIViewController {
     }
     
     //TODO:出発時刻を設定して決定ボタンを押したらDBに登録する
-    
     @IBAction func changeDate(_ sender: UIDatePicker) {
         
         print(sender)
@@ -36,24 +35,39 @@ class DepDateTimeViewController: UIViewController {
     }
     
     
-    //TODO：決定ボタンを押した時に「設定完了しました」のアクションシートを表示する。また「TOPに戻る」もボタンを押すと設定TOP画面に遷移する
-    
-    //決定ボタンが押された時に発動する
+    //TODO:「キャンセル」ボタンを追加する・・時間設定をやり直すためのキャンセル
+    //OKボタンが押された時に発動する
     @IBAction func tapDtm(_ sender: Any) {
     
         //アラートオブジェクトを作る
-        let alert = UIAlertController(title: "TOPに戻ります", message: "よろしいですか？", preferredStyle: .alert)
+        let alert = UIAlertController(title: "出発時間を設定します", message: "よろしいですか？", preferredStyle: .alert)
         
         //OKが押されたときにmyMessageの中に書いた処理を実行するように設定している
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in self.myMessage()}))
         
+        //アラート画面を表示する
         present(alert,animated: true)
         
     
     }
     
+    //OKが押されたときにTOP画面に遷移するの中に書いた処理を実行するように設定
     func myMessage(){
         print("OK押されました")
+        
+        //アラート画面を
+        let alert = UIAlertController(title: "設定が完了しました", message: "TOPに戻ります。よろしいですか？", preferredStyle: .alert)
+        
+        //OKが押されたときにmyMessageの中に書いた処理を実行するように設定している
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in self.myMoveTOP()}))
+        
+        //アラート画面を表示する
+        present(alert,animated: true)
+        
+    }
+    
+    func myMoveTOP(){
+        print("TOPに戻ります")
     }
     
     
